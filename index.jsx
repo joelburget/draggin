eebfde76415e0cce5d558649c0aa2403bfebfc1e
Joel Burget <joel@khanacademy.org>
2014-06-07 22:59:37 -0700

    Remove some unused stuff

--- index.jsx
+++ index.jsx
@@ -3,7 +3,6 @@
  */
 
 var React = require('react');
-var mori = require('mori');
 var $ = require('jquery');
 
 var ReactART = require('react-art');
@@ -63,99 +62,12 @@ var Func = React.createClass({
     }
 });
 
-var MySurface = React.createClass({
-    render: function() {
-        return <Surface width={700}
-                        height={700}
-                        style={{cursor: 'pointer'}}>
-            {this.props.children}
-        </Surface>;
-    },
-
-    getInitialState: function() {
-        return {
-            x: 100,
-            y: 100
-        };
-    },
-
-    handleMouseDown: function(event) {
-        update(mori.assoc(globalState,
-            "mouseDown", true
-        ));
-    },
-
-    handleMouseUp: function(event) {
-        update(mori.assoc(globalState,
-            "mouseDown", false
-        ));
-    },
-
-    handleMouseMove: function(event) {
-        update(mori.assoc(globalState,
-            "mouseX", event.x,
-            "mouseY", event.y
-        ));
-    },
-
-    handleResize: function(e) {
-        update(mori.assoc(globalState,
-            "windowWidth", window.innerWidth,
-            "windowHeight", window.innerHeight
-        ));
-    },
-
-    componentDidMount: function() {
-        window.addEventListener('resize',    this.handleResize);
-        window.addEventListener('mousedown', this.handleMouseDown);
-        window.addEventListener('mouseup',   this.handleMouseUp);
-        window.addEventListener('mousemove', this.handleMouseMove);
-    },
-
-    componentWillUnmount: function() {
-        window.removeEventListener('resize', this.handleResize);
-        window.removeEventListener('mousedown', this.handleMouseDown);
-        window.removeEventListener('mouseup',   this.handleMouseUp);
-        window.removeEventListener('mousemove', this.handleMouseMove);
-    }
-});
-
-var Batching = require("react-raf-batching/ReactRAFBatching.js");
-Batching.inject();
-
-// application state holds:
-// - functions and data structures in scope
-//   - shape
-//   - position
-// - current mode: freestyle | searching (is that all?)
-// - mouse state
-//   - position
-//   - dragging?
-var globalState = mori.hash_map(
-    "mouseX", 0,
-    "mouseY", 0,
-    "windowWidth", window.innerWidth,
-    "windowHeight", window.innerHeight
-);
-
-// is this necessary? won't it update anyway?
-var requireUpdate = false;
-
 var testobj = require('./testjson.js');
 
-// transact in om
-var update = function(newState) {
-    requireUpdate = true;
-    globalState = newState;
-
-    var tm = PTerm(testobj, null);
-    React.renderComponent(
-        tm,
-        document.getElementById("main")
-    );
-};
-
-update(globalState);
+React.renderComponent(
+    PTerm(testobj, null),
+    document.getElementById("main")
+);
 
 // $.ajax('http://localhost:4296', {
 //     data: '{"tag":"SearchType","contents":"++"}',
@@ -166,9 +78,6 @@ $.ajax('http://192.168.8.114:4296', {
 .then(
     resp => {
         console.log("search came back: ", resp);
-        update(mori.assoc(globalState,
-            "searchResults", resp
-        ));
     }
 );
 
